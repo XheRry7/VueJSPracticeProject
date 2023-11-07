f4c05e53cfc815cc3dd41e1bd3416d3b09c4fd4f
XheRry7 <shehryarahmed1744@gmail.com>
2023-11-07 18:19:33 -0500

    fixed api endpoint and completed vue course

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export class UserService {
   static getAllUsers() {
-    let api = "https://jsonplaceholder.typicode.com/userss";
+    let api = "https://jsonplaceholder.typicode.com/users";
     return axios.get(api);
   }
 }
